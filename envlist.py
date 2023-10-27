e787365bca17aaca4cc663f34684276f28350120
zsb258 <zhuang.shengbin@u.nus.edu>
2023-10-27 09:08:53 +0000

    lab7 step 13

--- envlist.py
+++ envlist.py
@@ -43,6 +43,7 @@ with lock:
         # create a list (named clist) of nevns environments with the 
         # prefix envprefix
         # add code here
+        clist = [f'{envprefix}{n}' for n in range(int(nenvs))]
     else:
         # load hickle file
         clist = hickle.load(file_path)
@@ -50,9 +51,13 @@ with lock:
         if pmode == WRITE_MODE:
             # append item to end of list
             # add code here
+            assert env, 'env name cannot be empty'
+            clist.append(env)
         else:    
             # get and remove env from clist
             # add code here
+            assert len(clist) > 0, 'no available env'
+            env = clist.pop(0)
             # return env name
             print(env)
 
