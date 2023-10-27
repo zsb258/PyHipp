17048bc17f6724d0f73100d4bb37b741bdfe4cb6
zsb258 <zhuang.shengbin@u.nus.edu>
2023-10-27 09:10:10 +0000

    lab7 step 15

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -13,6 +13,12 @@
 #SBATCH -e rplhighpass-sort-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -21,3 +27,7 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
+
