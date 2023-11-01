72b09c8c8e42dbd4710f2927e6fa984bf0d8313a
shihchengyen <shihcheng@nus.edu.sg>
2023-11-02 02:52:04 +0800

    Added files created in Lab 7.

--- envlist.py
+++ envlist.py
@@ -20,39 +20,31 @@ if nargs > 3:
     print('Usage: envlist; envlist env; envlist envprefix nenvs')
     exit
 elif nargs > 2:
-    # creates or re-creates the list of environments
     pmode = RESET_MODE
-    # get prefix for environment name
     envprefix = sys.argv[1]
-    # get number of environments to create
     nenvs = sys.argv[2]
 elif nargs > 1:
-    # returns env to the list of environments
     pmode = WRITE_MODE
-    # get name of environment to return
     env = sys.argv[1]
 else:
-    # gets name of an environment to use
     pmode = READ_MODE
 
-# creates a lock for the file so it can only be accessed one at a time
 lock = FileLock(lock_path, timeout=time_out_secs)
 
 with lock:
     if pmode == RESET_MODE:
-        # create a list (named clist) of nevns environments with the 
-        # prefix envprefix
-        # add code here
+        clist1 = [*range(0,int(nenvs),1)]
+        clist = [envprefix + str(s) for s in clist1]
     else:
         # load hickle file
         clist = hickle.load(file_path)
 
         if pmode == WRITE_MODE:
             # append item to end of list
-            # add code here
+            clist.append(env)
         else:    
-            # get and remove env from clist
-            # add code here
+            # pop first item off list
+            env = clist.pop(0)
             # return env name
             print(env)
 
---
+++ sort-slurm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -J "sort"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o sort-slurm.%N.%j.out # STDOUT
+#SBATCH -e sort-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
+python -u -c "import PyHipp as pyh; import time; from PyHipp import mountain_batch; mountain_batch.mountain_batch(); from PyHipp import export_mountain_cells; export_mountain_cells.export_mountain_cells(); print(time.localtime());"
+
+conda deactivate
+/data/src/PyHipp/envlist.py $envarg
