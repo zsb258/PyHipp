f42700b2960efb5883fee59da654ac1d06f1c5e6
zsb258 <zhuang.shengbin@u.nus.edu>
2023-11-02 09:10:00 +0000

    lab8 step 11

---
+++ fsall-slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -J "fsall"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o fsall-slurm.%N.%j.out # STDOUT
+#SBATCH -e fsall-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; \
+import time; \
+import DataProcessingTools as DPT; \
+lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
+lfall.save(); \
+hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
+hfall.save();
+print(time.localtime());"
+
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:648088877685:awsnotify --message "FSJobDone"
